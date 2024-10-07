0162981651fcac8a1581cac136fb49ea40837b7a
lucnguyendss <luc.nguyen@dssolution.jp>
2024-10-07 10:48:04 +0700

    Test

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        // int test = 0;
+        // test = 'abc';
+        System.out.println( "Hello World!" );
+    }
+}
