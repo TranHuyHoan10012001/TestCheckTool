544033e213a8bdbd5ccba410374a386cbb990d86
lucnguyendss <luc.nguyen@dssolution.jp>
2024-10-07 10:56:03 +0700

    Fail test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        // int test = 0;
-        // test = 'abc';
+        int test = 0;
+        test = 'abc';
         System.out.println( "Hello World!" );
     }
 }
